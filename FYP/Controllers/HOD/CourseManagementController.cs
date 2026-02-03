a69b749343ee58f6b3bc9dae551978cc44557790
Huzaifa Abbasi <huzaifaabbasi351@gmail.com>
2026-02-03 10:26:43 +0500

    COurseManagementCOntroller

---
+++ FYP/Controllers/HOD/CourseManagementController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FYP.Models;
+
+namespace FYP.Controllers.HOD
+{
+    [RoutePrefix("api/CourseManagement")]
+    public class CourseManagementController : ApiController
+    {
+
+        FYPEntities db= new FYPEntities();
+
+        [HttpGet]
+        [Route("EnrollmentCourses/{sessionId}")]
+        public IHttpActionResult GetEnrollmentCourses(int sessionId)
+        {
+            var data = (from e in db.Enrollment
+                        join t in db.Teacher
+                            on e.teacherID equals t.userID
+                        join c in db.Course
+                            on e.courseCode equals c.code
+                        where e.sessionID == sessionId
+                        select new
+                        {
+                            id = e.id,
+                            teacher = t.name,
+                            course = c.title,
+                            code = e.courseCode
+                        }).ToList();
+
+            return Ok(data);
+        }
+
+    }
+}
